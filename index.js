70d8d7738d6ef0c2b0d6ae23986745a66d4b7a15
Your Name <pau.emd@gmail.com>
2021-12-02 11:03:56 -0300

    feat: fetch

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-module.exports = () => {
-  // ...
-};
+import fetch from "node-fetch";
+
+// module.exports = () => {
+//   // ...
+// };
+//usar fetch
+fetch('http://example.com/movies.json')
+  .then(response => console.log(response.status))
+  .catch(error => console.log(error.code))
+  //hola
