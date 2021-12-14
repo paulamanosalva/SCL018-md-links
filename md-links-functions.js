cb0efa8e93cddb8249ac3b889230d33d68805972
Your Name <pau.emd@gmail.com>
2021-12-14 12:01:28 -0300

    validate sin mostrar datos

--- md-links-functions.js
+++ md-links-functions.js
@@ -1,119 +1,77 @@
-// import fetch from "node-fetch";
-// import fs from 'fs';
-// import path from "path";
-
-// const userPath = process.argv[2];
-// let validate = true;
-// let stats = false;
-
-
-// //leer archivo
-// const readFileData = (fileToRead) => {
-//   const extension = path.extname(fileToRead);//guarda le ext del archivo en una variable
-//   if (extension === '.md') { //verifica si el archivo es md
-//     fs.readFile(fileToRead, 'utf8', (err, data) => { //si es md --> lee el contenido del archivo 
-//       if (err) throw err;
-//       const links = searchLinks(data) // ve si hay links y guarda en un array las coincidencias (todas)
-//       console.log(links); // sería allLinks que es el return de searchLinks, donde se guardó la info.
-//       return links;
-//     });
-//   }else {
-//     console.log('el archivo no es markdown');    
-//   }
-// };
-// const searchLinks = (data) => {
-//   const matchLinks = data.matchAll(/(?<!\!)\[([^\]]+)\]\((https?:\/\/[^\s)]+)\)/g)
-//   // constante que compara la data con los link
-//   const allLinks = [] 
-
-//       for (const match of matchLinks) { 
-//        const data = {
-//         href: match[2],
-//         text: match[1],
-//         file: userPath,
-//       }; 
-      
-//       allLinks.push(data);
-      
-//     }
-    
-//    return allLinks;
-  
-// };
-
-
-
-// const mdLinks = (userInput, options) => {
-//   readFileData(userInput);
-// }
-
-// mdLinks(userPath); //al correr el archivo se ejecuta mdLinks con el archivo que entrega el user
-
 import fetch from "node-fetch";
 import fs from 'fs';
-import path from "path";
+import path, { resolve } from "path";
 
 const userPath = process.argv[2];
 let validate = true;
 let stats = false;
 
+
+//leer archivo
 const readFileData = (fileToRead) => {
   const extension = path.extname(fileToRead);//guarda le ext del archivo en una variable
   if (extension === '.md') { //verifica si el archivo es md
-    fs.readFile(fileToRead, 'utf8', (err, data) => { //si es md --> lee el contenido del archivo 
-      if (err) throw err;
-      const links = searchLinks(data) // ve si hay links y guarda en un array las coincidencias (todas)
-      console.log(links); // sería allLinks que es el return de searchLinks, donde se guardó la info.
-      return links;
-    });
+    const data = fs.readFileSync(fileToRead, {encoding:'utf8', flag:'r'}) // es md --> lee el contenido del archivo 
+      return searchLinks(data); // ve si hay links y guarda en un array las coincidencias (todas)
+     
+
   }else {
     console.log('el archivo no es markdown');    
   }
 };
-const searchLinks = async (data) => {
-  const matchLinks = data.matchAll( /(?<!\!)\[([^\]]+)\]\((https?:\/\/[^\s)]+)\)/g);// constante que compara la data con los link
+const searchLinks = (data) => {
+  const matchLinks = data.matchAll(/(?<!\!)\[([^\]]+)\]\((https?:\/\/[^\s)]+)\)/g)
+  // constante que compara la data con los link
+  const allLinks = [] 
 
-   async function pushData(){
-     try {
-      const allLinks = []
-       for (const match of matchLinks) { 
-       let res = await validateLinks(match[2]);
+      for (const match of matchLinks) { 
        const data = {
         href: match[2],
         text: match[1],
         file: userPath,
-        status: res,
       }; 
-      
       allLinks.push(data);
-      console.log(allLinks);
     }
-    return allLinks
-     }
-     catch(e){
-      console.log(e);
-    }
-   
-   }
-  let result = await pushData();
-  return result;
-  
+   return allLinks;
 };
 
-
-
-
-async function validateLinks(url) {
-  const response = await fetch(url);
-  const linkStatus = await response.status;
-  return linkStatus;
-}
- 
-
-
-
-const mdLinks = (userInput, options) => {
-  readFileData(userInput);
+const fetchLinks = (url) =>{
+  return fetch(url)
+    .then(response => {status: response.status});
 }
 
-mdLinks(userPath); //al correr el archivo se ejecuta mdLinks con el archivo que entrega el user
+const validateLinks = (links) => {
+
+const fetched = []
+  for (let i= 0; i < links.length; i++) {
+    let link = links[i].href;
+    fetched.push(fetchLinks(link));
+
+  }
+  return Promise.all(fetched)
+
+};
+
+        
+
+function mdLinks(fileToRead){
+  return new Promise((resolve, reject)=>{
+    const links = readFileData(fileToRead);
+    if(!validate){
+      resolve(links)
+    }
+    else {
+    links.forEach((link, index) =>{
+      Object.assign(link, validateLinks(links)[index])
+    })
+    resolve(links);
+    
+    }
+    
+  })
+}
+
+  mdLinks(userPath)
+  .then(results => console.log(results))
+
+
